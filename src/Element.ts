9ae433e12829cf2c1f6dbb3eabfeff12e4725ebf
Oscar Lindqvist <olindqv@gmail.com>
2022-08-28 07:42:37 -0700

    Create simple html generator

--- src/Element.ts
+++ src/Element.ts
@@ -1,13 +1,42 @@
 export default class Element {
   private tag: string;
-  private children: Element[];
+  private children: Array<Element | string>;
+  private attributes: Map<string, string>;
 
-  constructor(tag: string, children?: Element[]) {
+  constructor(tag: string) {
     this.tag = tag;
-    this.children = children ?? [];
+    this.children = [];
+    this.attributes = new Map();
+  }
+
+  private getAttr() {
+    if (this.attributes.size === 0) {
+      return "";
+    }
+    return [...this.attributes].map(([key, value]) => `${key}="${value}"`).join(" ");
+  }
+
+  public set(attributes: Record<string, string>): Element;
+  public set(attribute: string, value: string): Element;
+  public set(a: any, b?: any) {
+    if (typeof a === "object") {
+      Object.keys(a).forEach((key) => {
+        this.attributes.set(key, a[key]);
+      });
+    } else if (typeof a === "string" && typeof b === "string") {
+      this.attributes.set(a, b);
+    }
+    return this;
+  }
+
+  public with(...children: Array<Element | string>) {
+    this.children = children;
+    return this;
   }
 
   public render(): string {
-    return `<${this.tag}>${this.children.map((child) => child.render())}</${this.tag}>`;
+    return `<${this.tag} ${this.getAttr()}>${this.children
+      .map((child) => (typeof child === "string" ? child : child.render()))
+      .join("")}</${this.tag}>`;
   }
 }
